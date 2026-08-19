990a646cf0c3d479d5f2d69e63c983d2c3024e6d
Brent Bishoff <44077242+bdbishoff@users.noreply.github.com>
2026-08-19 12:19:14 -0700

    Add: two-integer-sum - submission-14

---
+++ Data Structures & Algorithms/two-integer-sum/submission-14.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # solution one n2 brute force 
+        # 2 pointer n
+        # solution 3: dictionary keying, also n
+        dind = dict()
+        for i in range(len(nums)):
+            dind[nums[i]] = i
+        for i in range(len(nums)):
+            curr = target - nums[i]
+            if curr in dind and dind[curr] != i:
+                print(curr, dind[curr])
+                return [i, dind[curr]]
+        
